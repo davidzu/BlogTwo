97506be5517c1301ef9191a11fbe7ccab657ee2e
Davidzu <davidzuc@gmail.com>
2021-09-09 14:10:52 -0500

    added enum function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :reviews
-  resources :blogs
+  resources :blogs do
+  	member do
+  		post :toggle_status
+  	end
+  end
   
   get 'about', to: 'pages#about'
   root to: 'pages#home'
